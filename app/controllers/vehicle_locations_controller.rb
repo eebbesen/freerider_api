5d3196860069daf89adb9999e5edde7b8180f075
Eric Ebbesen <eebbesen.git@gmail.com>
2016-02-20 09:35:26 -0600

    Removing operations I don't want to expose

--- app/controllers/vehicle_locations_controller.rb
+++ app/controllers/vehicle_locations_controller.rb
@@ -1,7 +1,7 @@
 class VehicleLocationsController < ApplicationController
   include DropboxPersistence
 
-  before_action :set_vehicle_location, only: [:show, :update, :destroy]
+  before_action :set_vehicle_location, only: [:show]
 
   # GET /vehicle_locations
   # GET /vehicle_locations.json
@@ -15,37 +15,6 @@ class VehicleLocationsController < ApplicationController
   # GET /vehicle_locations/1.json
   def show
     render json: @vehicle_location
-  end
-
-  # POST /vehicle_locations
-  # POST /vehicle_locations.json
-  def create
-    @vehicle_location = VehicleLocation.new(vehicle_location_params)
-    if @vehicle_location.save
-      render json: @vehicle_location, status: :created, location: @vehicle_location
-    else
-      render json: @vehicle_location.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /vehicle_locations/1
-  # PATCH/PUT /vehicle_locations/1.json
-  def update
-    @vehicle_location = VehicleLocation.find(params[:id])
-
-    if @vehicle_location.update(vehicle_location_params)
-      head :no_content
-    else
-      render json: @vehicle_location.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /vehicle_locations/1
-  # DELETE /vehicle_locations/1.json
-  def destroy
-    @vehicle_location.destroy
-
-    head :no_content
   end
 
   # saves each VehicleLocation to a database
--- test/controllers/vehicle_locations_controller_test.rb
+++ test/controllers/vehicle_locations_controller_test.rb
@@ -30,41 +30,9 @@ class VehicleLocationsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:vehicle_locations)
   end
 
-  test 'should create vehicle_location' do
-    assert_difference('VehicleLocation.count') do
-      post :create, vehicle_location: { vehicle: 'CCC333', latitude: 1, longitude: -2.3456789, location: 'twincities', vin: 'ABCD0000000000001' }
-    end
-
-    assert_response 201
-  end
-
-  test 'fail on create' do
-    post :create, vehicle_location: { vehicle: nil }
-    assert_response :unprocessable_entity
-  end
-
   test 'should show vehicle_location' do
     get :show, id: @vehicle_location
     assert_response :success
-  end
-
-  test 'should update vehicle_location' do
-    put :update, id: @vehicle_location, vehicle_location: { vehicle: 'AAA000', latitude: 3, longitude: -2.3456789, location: 'twincities', vin: 'ABCD0000000000001' }
-    assert_response 204
-    assert_equal(3, VehicleLocation.find_by_id(@vehicle_location).latitude)
-  end
-
-  test 'fail on update' do
-    patch :update, id: @vehicle_location, vehicle_location: { vehicle: nil }
-    assert_response :unprocessable_entity
-  end
-
-  test 'should destroy vehicle_location' do
-    assert_difference('VehicleLocation.count', -1) do
-      delete :destroy, id: @vehicle_location
-    end
-
-    assert_response 204
   end
 
   test 'should query car2go and persist records' do
