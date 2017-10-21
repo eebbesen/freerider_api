8b65892e48715710c9ff05db6cf01e75a349d8e3
Eric Ebbesen <eebbesen.git@gmail.com>
2017-10-21 16:27:45 -0500

    For prod Heroku compatibility

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.dropbox_token = ENV['DROPBOX_OAUTH_BEARER']
+  config.dropbox_data_token = ENV['DROPBOX_OAUTH_BEARER']
 end
